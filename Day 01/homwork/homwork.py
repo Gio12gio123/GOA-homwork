b0cd3de2d590bc18569c18a134db750fbfebcf9f
Gio12gio123 <izoriagio1@gmail.com>
2024-06-24 17:57:27 +0400

    second commit

--- Day 01/homwork/homwork.py
+++ Day 01/homwork/homwork.py
@@ -1,6 +1,35 @@
-from turtle import *
+name = "gio"
+#naimi aris cvladi
 
-forward(200)
 
-exitonclick()
+
+surname = "izoria"
+
+print (name)
+
+
+name = 'gio' # es aris str (string) tipis
+age=12 # es aris int (integer)
+height = 159 # es aris float
+#boolen (bool)
+
+knows_programing = False
+likes_tennis = True
  
+
+
+
+print(name +" " +surname)
+
+#print(name + age)
+
+
+#print (type(age))
+#print (type(name))
+#print (type(surname))
+#print (type(height))
+
+
+
+#i am gio izoria  i am 12 years old my height is 155cm  i love programing
+
