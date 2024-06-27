542e48ff7d25c025755d9020bc0dff8902a965de
Gio12gio123 <izoriagio1@gmail.com>
2024-06-27 15:09:05 +0400

    second commit

---
+++ Day 03/Homework/homwork.py
@@ -0,0 +1,69 @@
+#1) შექმენი ცვლადები,  სახელი,გვარი,ასაკი,ჰობი,ქვეყანა და  ეს ყველა ცვლადი დაპრინტეთ. 
+
+name = " giorgi "
+last_name = " izoria "
+hobby = " tennis "
+country = " georgia " 
+
+print(name)
+print(last_name)
+print(hobby)
+print(country)
+
+
+# 2) შექმენით ცვლადები, გადაეცით მნიშვნელობები და შემდგომ ააწყვეთ ერთი დიდი წინადადება.
+
+name = " giorgi "
+last_name = " izoria "
+hobby = " programing "
+country = " georgia "
+
+print(" my name is " + name + " my last name is " + last_name + " i am frome " + country + " my hobby is " + hobby)
+
+
+
+
+#3)  გააკეთეთ 20 მაგალითი ყველა მათემათიკურ ოპერატორებზე. +,-,*,//,**
+
+
+sum = 20 + 40
+print (sum)
+multiplay = 20 * 12
+print(multiplay)
+multiplay_2 = 20 ** 12
+print(multiplay_2)
+devision = 12 // 3
+print(devision)
+devision_2 = 12 / 3
+print(devision_2)
+minus = 780-291
+print(minus)
+sum_2 = 231 + 321
+print(sum_2)
+minus_2 = 1203-1202
+print(minus_2)
+devision_3 = 72 / 26
+print(devision_3)
+devision_4 = 72 // 26
+print (devision_4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
