11f3650114c38867de6c809097ce6cbec13b82e1
Gio12gio123 <izoriagio1@gmail.com>
2024-08-10 17:03:33 +0400

    second commit

---
+++ Day 18/homwork/homwork.py
@@ -0,0 +1,17 @@
+for i in range(10):
+    print("i goa")
+
+for i in range(51,1,1):
+    print(i)
+
+    for i in range(51,1,2):
+        print(i)
+
+        for i in range(51,1,3):
+            print(i)
+
+            for i in range(51,1,4):
+                print(i)
+   
+
+   
---
+++ Day 21/classwork/classwork.py
@@ -0,0 +1,15 @@
+x = 0
+while x <= 10:
+    x = x + 1
+    print("goa best")
+
+for i in range(1,50+1,1):
+    print(i)
+
+y = 12
+while y <= 30:
+    y = y+1
+    print("programing")
+
+for i in range(0,40+1,3):
+    print(i)
