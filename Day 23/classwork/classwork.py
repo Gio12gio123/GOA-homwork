08a006b7d88ffe06c06d4ab338b3582dd88b242d
Gio12gio123 <izoriagio1@gmail.com>
2024-08-13 13:59:04 +0400

    second commit

---
+++ Day 23/classwork/classwork.py
@@ -0,0 +1,16 @@
+Data=["titanic","pinguns","nun2","harry potter","indian"]
+
+
+Data[0]="carlson"
+Data[1]="hood"
+Data[4]="indian2"
+print(Data)
+
+
+data=[1,2,3,4,5,6,7,8,9,10]
+print(data[0],data[1],data[2],data[3],data[4],data[5],data[6],data[7],data[8],data[9])
+
+
+
+data=["gamarjoba","me","mqvia","giorgi","sasiamovnoa","sheni gacnoba"]
+print(data[0]+data[1]+data[2]+data[3]+data[4]+data[5])
